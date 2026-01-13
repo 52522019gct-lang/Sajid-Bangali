690cc1d1551f7966cd16e82e3b924f0bdf0a67bf
Mateen <mateeniqbal5252@gmail.com>
2026-01-13 19:02:22 +0500

    Initial commit: add HTML, CSS, JS project

---
+++ script.js
@@ -0,0 +1,9 @@
+// صفحہ لوڈ ہونے پر
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("بابا ساجد شاہ کی ویب سائٹ لوڈ ہو گئی");
+});
+
+// واٹس ایپ اوپن کرنے کا فنکشن
+function openWhatsApp() {
+    window.open("https://wa.me/923214890705", "_blank");
+}
